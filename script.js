2edd644772a1b3865330b3b0fa4cb45a763ff419
Eyasin Sheikh Rony <100122939+RonyVau2573@users.noreply.github.com>
2023-06-11 14:02:52 +0600

    Our Latest Articles Done

--- script.js
+++ script.js
@@ -25,6 +25,24 @@ const slideImg = () => {
   });
 };
 
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 // Drop Down Menu
 function menu(e) {
   let list = document.querySelector("ul");
